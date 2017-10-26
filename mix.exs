5a34f877da0bff94ee01e0ebfa08332dad4c7a32
RaymondLoranger <Raymond.Loranger@bell.net>
2017-10-26 16:42:35 -0400

    also keywords...

--- mix.exs
+++ mix.exs
@@ -24,7 +24,6 @@ defmodule MapSorter.Mixfile do
     """
     Sorts a list of maps¹ as per a list of sort specs
     (ascending/descending keys).
-
     ¹Also keywords or structures implementing the Access behaviour.
     """
   end
