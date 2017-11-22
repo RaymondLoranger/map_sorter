6fe8b26405bc3472c57dddf3d83ac6b20ab73f18
RaymondLoranger <Raymond.Loranger@bell.net>
2017-11-21 21:06:37 -0500

    mix tasks dep

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MapSorter.Mixfile do
   def project() do
     [
       app: :map_sorter,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       name: "Map Sorter",
@@ -46,6 +46,7 @@ defmodule MapSorter.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps() do
     [
+      {:mix_tasks, path: "../mix_tasks", only: :dev, runtime: false},
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false}
