e538341d7145d84b2b48fd07aaf73da52c5ee831
RaymondLoranger <Raymond.Loranger@bell.net>
2018-01-31 19:37:25 -0500

    mix format

--- config/config.exs
+++ config/config.exs
@@ -5,21 +5,6 @@ use Mix.Config
 # To allow mix messages in colors...
 config :elixir, ansi_enabled: true
 
-# Comment out to compile debug, info and warn messages...
-config :logger, compile_time_purge_level: :error
-
-# Prevents runtime debug, info and warn messages...
-config :logger, level: :error
-
-# Listed by ascending log level...
-config :logger, :console,
-  colors: [
-    debug: :light_cyan,
-    info: :light_green,
-    warn: :light_yellow,
-    error: :light_red
-  ]
-
 # When false (or nil), will simplify the compare function AST...
 # config :map_sorter, sorting_on_structs?: true
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MapSorter.Mixfile do
   def project do
     [
       app: :map_sorter,
-      version: "0.2.12",
+      version: "0.2.13",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       name: "Map Sorter",
