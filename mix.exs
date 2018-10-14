0fa6a18774d3791720c5e0ec5627eb5ff430ee90
RaymondLoranger <Raymond.Loranger@bell.net>
2018-10-14 18:18:49 -0400

    0.2.23

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MapSorter.Mixfile do
   def project do
     [
       app: :map_sorter,
-      version: "0.2.22",
+      version: "0.2.23",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "Map Sorter",
