45ea6925e215c9c0efea39d4e8bf8bdc8c28fcc8
RaymondLoranger <Raymond.Loranger@bell.net>
2021-12-03 14:34:20 -0500

    0.2.46

--- lib/map_sorter/log.ex
+++ lib/map_sorter/log.ex
@@ -4,111 +4,91 @@ defmodule MapSorter.Log do
   debug :sort_specs, {sort_specs, specs, env, caller} do
     """
     \nSort specs...
-    • Inside function:
-      #{fun(caller)}
-    • Inside macro:
-      #{fun(env)}
     • 'Received' sort specs:
       #{inspect(sort_specs)}
     • 'Adjusted' sort specs:
       #{inspect(specs)}
-    #{from()}
+    #{from(caller)}
+    • Macro: #{fun(env)}
     """
   end
 
   error :invalid_specs, {bad_specs, env, caller} do
     """
     \nSort 'declined' given invalid sort specs...
-    • Inside function:
-      #{fun(caller)}
-    • Inside macro:
-      #{fun(env)}
     • 'Invalid' sort specs:
       #{inspect(bad_specs)}
-    #{from()}
+    #{from(caller)}
+    • Macro: #{fun(env)}
     """
   end
 
   debug :compile_time_comp_fun_ast, {sort_specs, ast, env} do
     """
     \nAST of 'compile time' compare function...
-    • Inside function:
-      #{fun(env)}
     • Sort specs:
       #{inspect(sort_specs)}
     • AST:
       #{inspect(ast)}
-    #{from()}
+    #{from(env)}
     """
   end
 
   debug :runtime_comp_fun_ast, {sort_specs, ast, env} do
     """
     \nAST of 'runtime' compare function...
-    • Inside function:
-      #{fun(env)}
     • Sort specs:
       #{inspect(sort_specs)}
     • AST:
       #{inspect(ast)}
-    #{from()}
+    #{from(env)}
     """
   end
 
   debug :comp_fun_heredoc, {sort_specs, heredoc, env} do
     """
     \nHeredoc of compare function...
-    • Inside function:
-      #{fun(env)}
     • Sort specs:
       #{inspect(sort_specs)}
     • Heredoc:
       #{String.trim_trailing(heredoc) |> String.replace("\n", "\n  ")}
-    #{from()}
+    #{from(env)}
     """
   end
 
   debug :generating_compile_time_comp_fun, {sort_specs, env} do
     """
     \nGenerating 'compile time' compare function...
-    • Inside function:
-      #{fun(env)}
     • Sort specs:
       #{inspect(sort_specs)}
-    #{from()}
+    #{from(env)}
     """
   end
 
   debug :generating_runtime_comp_fun, {sort_specs, env} do
     """
     \nGenerating 'runtime' compare function...
-    • Inside function:
-      #{fun(env)}
     • Sort specs:
       #{inspect(sort_specs)}
-    #{from()}
+    #{from(env)}
     """
   end
 
   error :generating_no_op_sort, {sort_specs, env} do
     """
     \nGenerating 'no-op sort' as sort specs not a list...
-    • Inside function:
-      #{fun(env)}
     • Sort specs:
       #{inspect(sort_specs)}
-    #{from()}
+    #{from(env)}
     """
   end
 
   error :sort_specs_cannot_be, {sort_specs, what, env} do
     """
     \nSort specs cannot be #{what}...
-    • Inside function:
-      #{fun(env)}
     • 'Invalid' sort specs:
       #{inspect(sort_specs)}
-    #{from()}
+    #{from(env)}
     """
   end
 end
